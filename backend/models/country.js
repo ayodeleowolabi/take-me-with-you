e33a77d0a68f06e09a3ffc64a39803cf04099923
Ayodele Owolabi <joyowolabi@gmail.com>
2024-09-13 16:41:54 -0400

    Schema Build

---
+++ backend/models/country.js
@@ -0,0 +1,68 @@
+const mongoose = require('mongoose');
+// models/hoot.js
+
+const citySchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  historicalSites:{
+    type: String, 
+    required: false,
+  },
+  entertainment: {
+    type: String,
+    required: false
+  },
+  food: {
+    type: String,
+    required: false
+  },
+  timeSpent: {
+    type: String,
+    required: false
+  },
+  season: {
+    type: String,
+    required: false
+  },
+  rating: {
+    type: String, 
+    required: false,
+    enum:['*', '**','***','****','*****']
+  },
+  detailedRating: {
+    type: String,
+    required: false
+  }
+});
+
+
+
+const countrySchema = new mongoose.Schema({
+  name: {
+    type: String
+  }, 
+  continent: {
+    type: String,
+    required: false
+  },
+  city: [citySchema],
+  traveller: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User"
+  },
+},
+{timestamps: true}
+);
+
+
+
+// models/hoot.js
+
+  
+const Country = mongoose.model('Country', countrySchema)
+
+// models/hoot.js
+
+module.exports = Country;
--- backend/models/user.js
+++ backend/models/user.js
@@ -2,7 +2,11 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 const bcrypt = require('bcrypt');
 
+
+
+
 const SALT_ROUNDS = 6;
+
 
 const userSchema = new Schema(
   {
@@ -27,14 +31,13 @@ const userSchema = new Schema(
         return ret;
       },
     },
-  }
-);
+  },
 
 userSchema.pre('save', async function (next) {
   // 'this' is the user document
   if (!this.isModified('password')) return next();
   // Replace the password with the computed hash
   this.password = await bcrypt.hash(this.password, SALT_ROUNDS);
-});
+}));
 
 module.exports = mongoose.model('User', userSchema);
