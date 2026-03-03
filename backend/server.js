3bb2442c9bdcf4dc1cce6d4aab10b17adc44f012
Ayodele Owolabi <ayodeleowolabi@Ayodeles-MacBook-Pro.local>
2026-03-03 16:42:38 -0500

    removed error

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,6 @@
 // Process the secrets/config vars in .env
 console.log('PORT:', process.env.PORT);
-console.log('MONGO:', process.env.MONGODB_URI ? 'SET' : 'NOT SET');r
+console.log('MONGO:', process.env.MONGODB_URI ? 'SET' : 'NOT SET');
 equire('dotenv').config({ path: __dirname + '/.env' });
 
 const path = require('path'); // Built into Node
