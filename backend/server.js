2b4752b73dd6805bbfee3dfe0341a1c04ad33893
Ayodele Owolabi <ayodeleowolabi@Ayodeles-MacBook-Pro.local>
2026-03-03 16:20:00 -0500

    changed port

--- backend/server.js
+++ backend/server.js
@@ -47,7 +47,7 @@ app.get('*', function (req, res) {
   res.sendFile(path.join(__dirname, '../frontend/dist/index.html'));
 });
 
-const port = process.env.PORT || 3000;
-app.listen(port, () => {
-  console.log(`The express app is listening on ${port}`);
+const port = process.env.PORT || 8080;
+app.listen(process.env.PORT || 8080, () => {
+  console.log(`The express app is listening on ${process.env.PORT || 8080}`);
 });
