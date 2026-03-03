ea06024419edfc24d3c5faedbbe211040a356659
Ayodele Owolabi <ayodeleowolabi@Ayodeles-MacBook-Pro.local>
2026-03-03 17:27:07 -0500

    changed password in mongodo

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,5 @@
 // Process the secrets/config vars in .env
-console.log('PORT:', process.env.PORT);
-console.log('MONGO:', process.env.MONGODB_URI ? 'SET' : 'NOT SET');
+
 require('dotenv').config({ path: __dirname + '/.env' });
 
 const path = require('path'); // Built into Node
